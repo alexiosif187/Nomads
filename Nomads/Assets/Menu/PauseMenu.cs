87ecdd7ec30922a488a342a5dcc3e98444fb4255
alexiosif187 <88390154+alexiosif187@users.noreply.github.com>
2022-09-13 22:19:59 +0300

    Add files via upload

---
+++ Nomads/Assets/Menu/PauseMenu.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RTS;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GUISkin mySkin;
+    public Texture2D header;
+
+    private Player player;
+    private string[] buttons = { "Resume", "Exit Game", "Instructions" };
+
+    public AudioClip clickSound;
+    public float clickVolume = 1.0f;
+
+    private AudioElement audioElement;
+    void Start()
+    {
+        player = transform.root.GetComponent<Player>();
+        if (clickVolume < 0.0f) clickVolume = 0.0f;
+        if (clickVolume > 1.0f) clickVolume = 1.0f;
+        List<AudioClip> sounds = new List<AudioClip>();
+        List<float> volumes = new List<float>();
+        sounds.Add(clickSound);
+        volumes.Add(clickVolume);
+        audioElement = new AudioElement(sounds, volumes, "PauseMenu", null);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) Resume();
+    }
+
+    private void Resume()
+    {
+        Time.timeScale = 1.0f;
+        GetComponent<PauseMenu>().enabled = false;
+        if (player) player.GetComponent<UserInput>().enabled = true;
+        Cursor.visible = false;
+        ResourceManager.MenuOpen = false;
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = mySkin;
+
+        float groupLeft = Screen.width / 2 - ResourceManager.MenuWidth / 2;
+        float groupTop = Screen.height / 2 - ResourceManager.PauseMenuHeight / 2;
+        GUI.BeginGroup(new Rect(groupLeft, groupTop, ResourceManager.MenuWidth, ResourceManager.PauseMenuHeight));
+
+        //background box
+        GUI.Box(new Rect(0, 0, ResourceManager.MenuWidth, ResourceManager.PauseMenuHeight), "");
+        //header image
+        GUI.DrawTexture(new Rect(ResourceManager.Padding, ResourceManager.Padding, ResourceManager.HeaderWidth, ResourceManager.HeaderHeight), header);
+
+        //menu buttons
+        float leftPos = ResourceManager.MenuWidth / 3 - ResourceManager.ButtonWidth / 3;
+        float topPos = 3 * ResourceManager.Padding + header.height;
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (i > 0) topPos += ResourceManager.ButtonHeight + ResourceManager.Padding;
+            if (GUI.Button(new Rect(leftPos, topPos, ResourceManager.ButtonWidth, ResourceManager.ButtonHeight), buttons[i]))
+            {
+                switch (buttons[i])
+                {
+                    case "Resume": Resume(); break;
+                    case "Exit Game": ExitGame(); break; 
+                    case "Instructions": OpenTutorial(); break ;
+                    default: break;
+                }
+            }
+        }
+
+        GUI.EndGroup();
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenTutorial()
+    {
+        Time.timeScale = 0.0f;
+        GetComponent<PauseMenu>().enabled = false;
+        GetComponent<TutorialMenu>().enabled = true;
+        Cursor.visible = false;
+    }
+
+    private void PlayClick()
+    {
+        if (audioElement != null) audioElement.Play(clickSound);
+    }
+}
---
+++ Nomads/Assets/Menu/TutorialMenu.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RTS;
+
+public class TutorialMenu : MonoBehaviour
+{
+
+    public GUISkin mySkin;
+    public Texture2D header;
+
+    private Player player;
+    private string[] buttons = { "Back to Main Menu"};
+
+    void Start()
+    {
+        player = transform.root.GetComponent<Player>();
+    }
+
+    void Update()
+    { 
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = mySkin;
+
+        float groupLeft = Screen.width/10 - ResourceManager.MenuWidth/10;
+        float groupTop = Screen.height/10 - ResourceManager.PauseMenuHeight/10;
+        GUI.BeginGroup(new Rect(groupLeft, groupTop, ResourceManager.MenuWidth*3, ResourceManager.PauseMenuHeight*3));
+
+        //background box
+        GUI.Box(new Rect(0, 0, ResourceManager.MenuWidth*3, ResourceManager.PauseMenuHeight*3), "");
+        //header image
+        GUI.DrawTexture(new Rect(ResourceManager.Padding, ResourceManager.Padding, ResourceManager.HeaderWidth, ResourceManager.HeaderHeight), header);
+
+        //menu buttons
+        float leftPos = ResourceManager.MenuWidth / 2 - ResourceManager.ButtonWidth / 2;
+        float topPos = 2 * ResourceManager.Padding + header.height;
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (i > 0) topPos += ResourceManager.ButtonHeight + ResourceManager.Padding;
+            if (GUI.Button(new Rect(leftPos/3, topPos, ResourceManager.ButtonWidth*2-10, ResourceManager.ButtonHeight), buttons[i]))
+            {
+                switch (buttons[i])
+                {
+                    case "Back to Main Menu": BacktoMain(); break;
+                    default: break;
+                }
+            }
+            GUI.Button(new Rect(leftPos/3, topPos+ResourceManager.ButtonHeight+2, ResourceManager.MenuWidth*3-ResourceManager.Padding*3, ResourceManager.PauseMenuHeight*3-ResourceManager.Padding*10),
+                "Welcome and thank you for playing my game! \n\n" +
+                "Here are some basic controls:\n" +
+                "Move cursor to edges: moves camera left,right,forward,backwards.\n" +
+                "Scroll mouse: moves camera up and down\n" +
+                "Alt + left click: allows camera rotation (up,down,left,right)\n" +
+                "Right click: deselects object\n" +
+                "Left click (if none selected): selects object\n" +
+                "Left click (if blue tank selected): moves to indicated position on purple or blue tiles \n\n" +
+                "Left click (if blue tank or blue turrets selected): attacks enemy tank or turret\n" +
+                "Shift + right click: teleports the tank on blue tiles\n" +
+                "Escape: pauses and resumes game\n" +
+                "And some basic info to help you out:\n\n" +
+                "You can attack the enemy tanks and turrets, but this is not how you win.\n" +
+                "Move FAST and only destroy the 2 tanks in areas with a light shinning on them from above.\n" +
+                "Turrets can't move, but are very POWERFUL, watch out for them.\n" +
+                "You have your own turrets and should use them (e.g. lure tanks to them or attack enemy turrets).\n" +
+                "Your turrets won't always attack on their own, but you can direct them to attack what you want.\n"+
+                "The enemy tanks and turrets will only attack the player tank.You LOSE if your tank gets destroyed.\n" +
+                "You win the game by destroying the 2 tanks in brightly light-up areas.\n\n " +
+                "GOOD LUCK!"
+                );
+        }
+
+        GUI.EndGroup();
+    }
+
+    public void BacktoMain()
+    {
+        Time.timeScale = 0.0f;
+        GetComponent<TutorialMenu>().enabled = false;
+        GetComponentInChildren<PauseMenu>().enabled = true;
+        GetComponent<UserInput>().enabled = false;
+        ResourceManager.MenuOpen = true;
+    }
+
+}
